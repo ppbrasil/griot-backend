f5fec293c64693b83aa4e555119b9f82d564a7f7
Pedro Brasil <ppbrasil@gmail.com>
2023-05-27 21:11:43 -0300

    Update profile to save timzone and language

--- griot_backend/profiles/models.py
+++ griot_backend/profiles/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+TIMEZONE_CHOICES = [
+    ('UTC', 'Coordinated Universal Time'),
+    ('America/New_York', 'Eastern Time (US & Canada)'),
+    ('America/Chicago', 'Central Time (US & Canada)'),
+    ('America/Denver', 'Mountain Time (US & Canada)'),
+    ('America/Los_Angeles', 'Pacific Time (US & Canada)'),
+    ('Europe/London', 'Greenwich Mean Time (GMT)'),
+    ('Europe/Paris', 'Central European Time (CET)'),
+    ('Asia/Kolkata', 'Indian Standard Time (IST)'),
+    ('Australia/Sydney', 'Australian Eastern Standard Time (AEST)'),
+    ('America/Sao_Paulo', 'Brasilia Time (BRT)'),
+    ('Asia/Tokyo', 'Japan Standard Time (JST)'),
+    ('Pacific/Auckland', 'New Zealand Standard Time (NZST)'),
+]
+
+LANGUAGE_CHOICES = [
+    ('en', 'English'),
+    ('es', 'Spanish'),
+    ('pt', 'Portuguese'),
+]
+
+GENDER_CHOICES = [
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other')
+]
+
 class Profile(models.Model):
-    GENDER_CHOICES = [
-        ('male', 'Male'),
-        ('female', 'Female'),
-        ('other', 'Other')
-    ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
@@ -16,9 +38,14 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=False)
 
-    def __str__(self):
-        return self.user.username
+    language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES)
+    timezone = models.CharField(max_length=50, choices=TIMEZONE_CHOICES)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
 
     class Meta:
         verbose_name = 'Profile'
