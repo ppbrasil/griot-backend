021a0c07641a2e08ae6056b98ba08996d7370094
Pedro Brasil <ppbrasil@gmail.com>
2023-06-02 18:39:21 -0300

    Spli  docker-compose & settings for prod and dev

--- griot_backend/griot_backend/settings_dev.py
+++ griot_backend/griot_backend/settings_dev.py
@@ -29,12 +29,6 @@ SECRET_KEY = 'django-insecure-nk_g(*9p_rmk503c^rs+gihee$o)colhi9)t2asqv75w(*!ur-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [
-#     'localhost',
-#     '127.0.0.1',
-#     'griot-load-balancer-1658910802.us-east-1.elb.amazonaws.com',
-# ]
-
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -99,31 +93,16 @@ WSGI_APPLICATION = 'griot_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dbname',
-#         'USER': 'dbuser',
-#         'PASSWORD': 'dbpass',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'griotdb',
-        'USER': 'djangodbuser',
-        'PASSWORD': 'DB_Pass!',
-        'HOST': 'griot-database.cubmeht6dhdd.us-east-1.rds.amazonaws.com',
+        'NAME': 'dbname',
+        'USER': 'dbuser',
+        'PASSWORD': 'dbpass',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
